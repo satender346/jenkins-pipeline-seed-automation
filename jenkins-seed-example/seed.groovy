2271d3471bd5246dcb55e84103a44061b7f0994e
Satender Yadav <satender346@gmail.com>
2021-01-24 21:10:01 +0000

    Update jenkins build parameters to support DSL and update jenkins job
    Change-Id: If43a683bb78c8788cc85d336ed7be6c6835c032f

--- jenkins-seed-example/seed.groovy
+++ jenkins-seed-example/seed.groovy
@@ -1,19 +1,36 @@
 //Sample seed file to create jenkins job (sample-pipeline) in deployment folder
 // with paramters and environment variables.
-folder("deployment")
-JOB_NAME = "sample-pipeline"
-pipelineJob("${JOB_NAME}") {
-  displayName("Sample Jenkins deployment pipeline")
+JOB_FOLDER="deployment"
+folder("${JOB_FOLDER}")
+JOB_NAME = "cloud"
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+  displayName("Deployment pipeline")
   description("Sample jenkins deployment pipeline")
   logRotator{
      daysToKeep(90)
   }
-  stringParam('SITE_NAME', 'dev1', 'Site name')
-  stringParam('GIT_BRANCH', 'main', 'GIT Branch name')
-  booleanParam('EXECUTE_TEST_CASE', true, 'Run test case')
-  environmentVariables(
-    "FILE_PATH": "/var/jenkins_home/sample_file",
-  )
+  parameters {
+         string {
+            defaultValue("dev1")
+            description("Site name")
+            name("SITE_NAME")
+            trim(true)
+         }
+         string {
+              defaultValue("master")
+              description("Pegleg image")
+              name("PEGLEG_IMAGE")
+              trim(true)
+         }
+         booleanParam {
+                defaultValue(true)
+                description("Execute shipyard actions on genesis node using internal shipyard url.")
+                name("EXECUTE_TEST_CASE")
+        }
+       environmentVariables(
+          "FILE_PATH": "/var/jenkins_home/sample_file"
+       )
+  }
   concurrentBuild(false)
   triggers {
      definition {
