b6095a711eae42480a7ed41f5d97994588a328ee
Yadav, Satender (sy336r) <sy336r@att.com>
2021-01-27 16:19:20 -0600

    Adding image promotion to harbor registry pipeline
    1. Pipeline will promote image to harbor registry

---
+++ jenkins-harbor-image-promotion/seed.groovy
@@ -0,0 +1,41 @@
+//Sample seed file to create image promotion to harbor registry
+
+JOB_FOLDER="image"
+folder("${JOB_FOLDER}")
+JOB_NAME = "harbor-promotion"
+pipelineJob("${JOB_FOLDER}/${JOB_NAME}") {
+  displayName("Harbor image promotion")
+  description("Sample jenkins pipeline to promot image in harbor registry")
+  logRotator{
+     daysToKeep(90)
+  }
+  parameters {
+         string {
+            defaultValue("hello-world")
+            description("docker image example ")
+            name("IMAGE_NAME")
+            trim(true)
+         }
+         string {
+              defaultValue("admin")
+              description("Harbor user name")
+              name("HARBOR_USER_NAME")
+              trim(true)
+         }
+         nonStoredPasswordParam {
+              defaultValue("Harbor12345")
+              description("Harbor password")
+              name("HARBOR_USER_PASSWORD")
+        }
+  }
+  concurrentBuild(false)
+  triggers {
+     definition {
+       cps {
+            def jenkinsfile = readFileFromWorkspace("jenkins-harbor-image-promotion/Jenkinsfile")
+            script(jenkinsfile)
+            sandbox(true)
+          }
+        }
+     }
+}
